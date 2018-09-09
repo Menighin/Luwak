c8b9e650069809fcbc62396c2a1332c10f28fb3d
Menighin <joao.menighin@gmail.com>
2018-09-09 13:46:48 -0300

    Filter - In Progress

--- src/main/java/testapp/filters/CityPageFilter.kt
+++ src/main/java/testapp/filters/CityPageFilter.kt
@@ -1,12 +1,16 @@
 package testapp.filters
 
 import com.menighin.luwak.core.annotations.Label
+import com.menighin.luwak.core.annotations.LuwakFilter
 import com.menighin.luwak.core.interfaces.ILuwakFilter
 
+@LuwakFilter
 class CityPageFilter : ILuwakFilter {
 
+    @Label("Code")
     var cityCode: String = ""
 
+    @Label("Country")
     var countryCode: String = ""
 
 }
